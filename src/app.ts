fb5d995421fa63ea4fdbb871f60a485d3e2b5aa6
ilia-devfamily <i.mazhugin@dev.family>
2023-04-22 23:21:30 +0300

    adde .env.sample

--- src/app.ts
+++ src/app.ts
@@ -6,12 +6,12 @@ config();
 
 export const startServer = () => {
   const httpServer = createHttpServer();
-
   const wss = createWSServer(Number(process.env.WS_PORT));
 
-  httpServer.listen(process.env.HTTP_PORT);
-  wss.on("connection", stream => {});
+  httpServer.listen(process.env.HTTP_PORT, () => {
+    console.log(`Start static http server on the ${process.env.HTTP_PORT} port..`);
+  });
 
-  console.log(`Start static http server on the ${process.env.HTTP_PORT} port..`);
+  wss.on("connection", stream => {});
   console.log(`WebSocket server started on the ${process.env.WS_PORT} port...`);
 };
